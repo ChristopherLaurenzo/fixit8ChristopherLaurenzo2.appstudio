6b2a1013034ac1b53905a8ea8c58cff4adf3cea0
Christopher Laurenzo <cll54497@creighton.edu>
2019-09-29 12:32:44 -0500

    finished assignment

---
+++ fixit8ChristopherLaurenzo2.appstudio/forms/Calculate/Calculate.js
@@ -0,0 +1,19 @@
+function Calculate (x,y){
+
+  let summedNumbers = x+y
+
+  return summedNumbers
+
+  }
+
+ 
+
+ let x = Number(prompt( "What is the first number? "))
+
+let y = Number(prompt( "What is the second number? "))
+
+let myAnswer = Calculate(x,y)
+
+console.log(`The sum of ${x} and ${y} is ${myAnswer}`)
+
+ 
---
+++ fixit8ChristopherLaurenzo2.appstudio/forms/WhileExtra/WhileExtra.js
@@ -0,0 +1,12 @@
+let fullName = prompt(" What is your full name? ")
+
+let i = 0
+
+while(i < fullName.length) {
+
+  console.log(`The letter is ${i+1} is : ${fullName.charAt(i)}`)
+
+  i++
+
+  }
+  
